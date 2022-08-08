34e627a535d208bfd324ffaa70565a50f9078600
DevFaizaan <DevFaizaan.b@gmail.com>
2022-08-08 14:19:05 +0100

    test

---
+++ src/main/java/com/qa/july2022/July2022Application.java
@@ -0,0 +1,13 @@
+package com.qa.july2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class July2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(July2022Application.class, args);
+	}
+
+}
