12704fec40bd538a849199f2b28d14195c717bfd
DevFaizaan <DevFaizaan.b@gmail.com>
2022-08-09 17:01:03 +0100

    springboot stufff

--- src/main/java/repos/JuiceRepo.java
+++ src/main/java/repos/JuiceRepo.java
@@ -1,5 +1,7 @@
 package repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import domain.Juice;
 
 @Repository
 public interface JuiceRepo extends JpaRepository<Juice, Integer> {
-
+	 Optional<Juice> existingOptional = this.repo.findById(id);
+     Juice existing = existingOptional.get();
 }
